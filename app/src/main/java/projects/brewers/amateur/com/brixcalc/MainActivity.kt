a5de245068da3b77bc87dbf81632acce1b8a5896
Alex Biehl <biehl.al@gmail.com>
2023-12-06 17:19:00 -0500

    added basic main activity

--- app/src/main/java/projects/brewers/amateur/com/brixcalc/MainActivity.kt
+++ app/src/main/java/projects/brewers/amateur/com/brixcalc/MainActivity.kt
@@ -10,7 +10,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.core.app.ComponentActivity
 import projects.brewers.amateur.com.brixcalc.ui.theme.BrixCalcTheme
 
 class MainActivity : ComponentActivity() {
